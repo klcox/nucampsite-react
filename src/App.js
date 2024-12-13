36e8ae9fde4f27191b8cc9978717247fbdaf437f
Kelly Cox <klcox@comcast.net>
2024-12-13 15:55:45 -0600

    homepage display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import {Routes, Route} from "react-router-dom";
+import ContactPage from "./pages/ContactPage.js";
+import HomePage from "./pages/HomePage.js";
+import Header from "./components/Header";
+import CampsiteDirectoryPage from "./pages/CampsiteDirectoryPage.js";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsiteDirectoryPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
